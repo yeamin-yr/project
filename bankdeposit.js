a9ed05b3ffd566b77de354a3537392d31ea17681
yeamin-yr <yeamin1666@gmail.com>
2021-01-24 13:45:29 +0600

    hii

---
+++ bankdeposit.js
@@ -0,0 +1,15 @@
+/*<div class="style1" class="col-md-6">
+<h2>Deposit</h2>
+
+<input type="number" placeholder="$ inter amount you want to deposit" class="form-control"><br>
+<button class="btn btn-success">Enter</button><br>
+
+
+
+</div>
+
+<div class="style1" class="col-md-6">
+<h2>wed</h2>
+
+
+</div> */
